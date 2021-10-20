0404350ba2ee0ccae08bbecf1984695ef4474215
unknown <quangder123@gmail.com>
2021-10-20 21:31:35 +0700

    sua lan 1

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -123,14 +123,12 @@
           <div class="col-xs-12 col-sm-12 col-md-2 animate-dropdown top-cart-row">
             <!-- ============================================================= SHOPPING CART DROPDOWN ============================================================= -->
 
-            <div class="dropdown dropdown-cart"> <a href="#" class="dropdown-toggle lnk-cart" data-toggle="dropdown"></a>
-            
+            <div class="dropdown dropdown-cart"> <a href="#" class="dropdown-toggle lnk-cart" data-toggle="dropdown">
                 <div class="items-cart-inner">
                   <div class="basket"> <i class="glyphicon glyphicon-shopping-cart"></i> </div>
                   <div class="basket-item-count"><span class="count">2</span></div>
                   <div class="total-price-basket"> <span class="lbl">cart -</span> <span class="total-price"> <span class="sign">$</span><span class="value">600.00</span> </span> </div>
                 </div>
-
               </a>
               <ul class="dropdown-menu">
                 <li>
